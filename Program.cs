7ea6ef6b5913bc00c260d335fc988b38c592818a
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-02-13 12:15:19 +0300

    Создал классы Заказов, Клиента, исполнителя и базовые функции для изменения статуса заказа и валидности введённого номера телефона у клиента и исполнителя

--- Program.cs
+++ Program.cs
@@ -13,11 +13,123 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
 }
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
+
+public class Client
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    private string _phoneNumber = "";
+    public string PhoneNumber
+    {
+        get { return _phoneNumber; }
+        set
+        {
+            ValidatePhoneNumber(value);
+            _phoneNumber = value;
+        }
+    }
+    public string? Email { get; set; }
+
+    public Client(string firstName, string lastName, string phoneNumber, string email)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+
+    private void ValidatePhoneNumber(string phoneNumber)
+    {
+        foreach (char symbol in phoneNumber)
+        {
+            if (char.IsLetter(symbol))
+                throw new Exception("Номер телефона должен состоять из цифр.");
+        }
+    }
+}
+
+public class Executor
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    private string _phoneNumber = "";
+    public string PhoneNumber
+    {
+        get { return _phoneNumber; }
+        set
+        {
+            ValidatePhoneNumber(value);
+            _phoneNumber = value;
+        }
+    }
+
+    public Executor(string firstName, string lastName, string phoneNumber)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+    }
+
+    private void ValidatePhoneNumber(string phoneNumber)
+    {
+        foreach (char symbol in phoneNumber)
+        {
+            if (!char.IsDigit(symbol))
+                throw new Exception("Номер телефона должен состоять из цифр.");
+        }
+    }
+}
+
+public class Order
+{
+    public int OrderNumber { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string Device { get; set; }
+    public string ProblemType { get; set; }
+    public string Description { get; set; }
+    public OrderStatus Status { get; set; }
+    public Client? Client { get; set; }
+    public Executor? Executor { get; set; }
+    public DateOnly? EndDate { get; set; } = null;
+    public List<string> Comments { get; set; } = [];
+
+    public Order(int orderNumber, DateTime orderDate, string device, string problemType, string description, OrderStatus status, Client client, Executor executor)
+    {
+        OrderNumber = orderNumber;
+        OrderDate = orderDate;
+        Device = device;
+        ProblemType = problemType;
+        Description = description;
+        Status = status;
+        Client = client;
+        Executor = executor;
+    }
+
+    public void ChangeStatus()
+    {
+        switch (Status)
+        {
+            case OrderStatus.WaitingForExecution:
+                Status = OrderStatus.InRepair;
+                break;
+            case OrderStatus.InRepair:
+                Status = OrderStatus.ReadyToIssue;
+                break;
+            case OrderStatus.ReadyToIssue:
+                Status = OrderStatus.WaitingForExecution;
+                break;
+        }
+    }
+}
+
+public enum OrderStatus
+{
+    WaitingForExecution,
+    InRepair,
+    ReadyToIssue
+}
+
